3c6975c339854a7ecbdb57076ae1c37e3e6786eb
Eralp Karaduman <eralpkaraduman@gmail.com>
2018-09-09 17:23:34 +0300

    Integrated material-ui

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import logo from './logo.svg';
 import './App.css';
 
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
 class App extends Component {
   render() {
+    const {classes} = this.props;
     return (
       <div className="App">
-        <header className="App-header">
+        <AppBar position='static'>
+          <Toolbar>
+            <Typography variant="title" color="inherit" className={classes.grow}>
+              Savings Projects
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
       </div>
     );
   }
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(App);
