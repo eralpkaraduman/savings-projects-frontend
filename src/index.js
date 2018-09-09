4ef02cc8195321c7aa3364b8cdc625a4993db0c6
Eralp Karaduman <eralpkaraduman@gmail.com>
2018-09-09 15:54:50 +0300

    Added polyfill for fetch

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'whatwg-fetch'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
