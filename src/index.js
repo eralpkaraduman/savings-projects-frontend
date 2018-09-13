5b5a126fae41b1cd4c4a70e1b734e854a4f5de91
Eralp Karaduman <eralpkaraduman@gmail.com>
2018-09-13 23:39:13 +0300

    Removed service worker

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import 'whatwg-fetch';
 import 'babel-polyfill';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
