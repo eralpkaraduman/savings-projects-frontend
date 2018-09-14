fb84971ab9179bd0996ed9b0eff95da5eab94c3d
Eralp Karaduman <eralpkaraduman@gmail.com>
2018-09-14 13:51:37 +0300

    Search field layout

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import styles from '../styles';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+import Input from '@material-ui/core/Input';
+import SearchIcon from '@material-ui/icons/Search';
+
+import styles from '../styles';
 
 class SearchField extends Component {
   render() {
     const { classes } = this.props;
     return (
-      <Paper className={classes.tableRoot}>
-        Search Field Will Be Here
+      <Paper className={ classes.searchContainer }>
+          <div className={ classes.search }>
+            <div className={ classes.searchIcon }>
+              <SearchIcon />
+            </div>
+            <Input
+              fullWidth
+              placeholder="Search…"
+              disableUnderline
+              classes={ {
+                root: classes.searchInputRoot,
+                input: classes.searchInputInput,
+              } }
+            />
+          </div>
       </Paper>
     );
   }
--- src/styles.js
+++ src/styles.js
@@ -1,3 +1,5 @@
+import { fade } from '@material-ui/core/styles/colorManipulator';
+
 const minPageWidth = 650;
 const pagePadding = 24;
 const minTableWidth = minPageWidth - pagePadding * 2;
@@ -11,7 +13,7 @@ const styles = theme => ({
   pageRoot: {
     flexGrow: 1,
     margin: 0,
-    padding: 10,
+    padding: theme.spacing.unit * 2,
     paddingTop: 0,
   },
   grow: {
@@ -26,7 +28,7 @@ const styles = theme => ({
   },
   tableRoot: {
     width: '100%',
-    marginTop: theme.spacing.unit * 3,
+    marginTop: theme.spacing.unit * 2,
     overflowX: 'auto',
   },
   tableRow: {
@@ -44,6 +46,50 @@ const styles = theme => ({
   rightToolbarItem: {
     marginLeft: 20,
     marginRight: -14,
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing.unit,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing.unit * 3,
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    margin: 0,
+    marginLeft: -2,
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  searchContainer: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 2,
+    overflowX: 'auto',
+  },
+  searchInputRoot: {
+    color: 'inherit',
+    width: '100%',
+  },
+  searchInputInput: {
+    paddingTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    paddingLeft: 30,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: 200,
+    },
   }
 });
 
